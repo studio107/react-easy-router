054956dab4b8c299a5bc15994adec378783b3d67
Falaleev Maxim <max@studio107.ru>
2017-10-02 21:19:08 +0300

    add react 16 support

--- src/Link.test.js
+++ src/Link.test.js
@@ -3,9 +3,14 @@ import { render } from 'react-dom';
 import Link from './Link';
 import Router from './Router';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import sinon from 'sinon';
 import createMemoryHistory from 'history/createMemoryHistory';
+
+configure({
+    adapter: new Adapter()
+});
 
 const history = createMemoryHistory();
 
@@ -24,22 +29,22 @@ describe('<Link />', () => {
     const router = shallow(<Router history={history} routes={routes}/>);
 
     it('render valid <Link /> component', () => {
-        const wrapper = shallow(<Link router={router} to="homepage" />);
+        const wrapper = shallow(<Link router={router} to="homepage"/>);
         expect(wrapper.prop('href')).to.equal('/');
     });
 
     it('renders children when passed in', () => {
         const wrapper = shallow(
             <Link router={router} to="homepage">
-                <div className="unique" />
+                <div className="unique"/>
             </Link>
         );
-        expect(wrapper.contains(<div className="unique" />)).to.equal(true);
+        expect(wrapper.contains(<div className="unique"/>)).to.equal(true);
     });
 
     it('click events', () => {
         const onClick = sinon.spy();
-        const wrapper = shallow(<Link history={history} router={router} to="homepage" onClick={onClick} />);
+        const wrapper = shallow(<Link history={history} router={router} to="homepage" onClick={onClick}/>);
         wrapper.simulate('click');
         expect(onClick).to.have.property('callCount', 1);
     });
